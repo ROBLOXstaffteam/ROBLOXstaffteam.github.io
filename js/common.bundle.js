22c77013552d20755e37f9853bd3a25f85db96ae
SoBloxsy <45794556+SoBloxsy@users.noreply.github.com>
2021-03-28 02:01:42 -0400

    Update common.bundle.js

--- js/common.bundle.js
+++ js/common.bundle.js
@@ -31,7 +31,7 @@ object-assign
 /** @license React v16.14.0
  * react-dom.production.min.js
  *
- * Copyright (c) Facebook, Inc. and its affiliates.
+ * Copyright (c) Face.book, Inc. and its affiliates.
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
